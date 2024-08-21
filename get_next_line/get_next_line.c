f840a3f16fd5724c0d867205478566a960c7964d
ludis <159616919+lud-is@users.noreply.github.com>
2024-08-21 23:21:27 +0200

    update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,66 +1,66 @@
 #include "get_next_line.h"
 
-char    *extract_line(char **remainder)
+char	*extract_line(char **remainder)
 {
-    size_t  len = 0;
-    char    *line;
-    char    *new_remainder;
+	size_t		len;
+	char	*line;
+	char	*new_remainder;
 
-    while ((*remainder)[len] && (*remainder)[len] != '\n')
-        len++;
-    line = ft_substr(*remainder, 0, len + ((*remainder)[len] == '\n'));
-    if (!line)
-        return (NULL);
-    if ((*remainder)[len] == '\n')
-        new_remainder = ft_strdup(*remainder + len + 1);
-    else
-        new_remainder = NULL;
-    free(*remainder);
-    *remainder = new_remainder;
-    return (line);
+	len = 0;
+	while ((*remainder)[len] && (*remainder)[len] != '\n')
+		len++;
+	line = ft_substr(*remainder, 0, len + ((*remainder)[len] == '\n'));
+	if (!line)
+		return (NULL);
+	if ((*remainder)[len] == '\n')
+		new_remainder = ft_strdup(*remainder + len + 1);
+	else
+		new_remainder = NULL;
+	free(*remainder);
+	*remainder = new_remainder;
+	return (line);
 }
 
-char    *ft_read_and_join(int fd, char *remainder)
+char	*ft_read_and_join(int fd, char *remainder)
 {
-    char    *buffer;
-    ssize_t bytes_read;
+	char	*buffer;
+	ssize_t		bytes_read;
 
-    buffer = malloc(BUFFER_SIZE + 1);
-    if (!buffer)
-        return (NULL);
-    bytes_read = read(fd, buffer, BUFFER_SIZE);
-    while (bytes_read > 0)
-    {
-        buffer[bytes_read] = '\0';
-        if (remainder)
-            remainder = ft_strjoin(remainder, buffer);
-        else
-            remainder = ft_strdup(buffer);
-        if (!remainder)
-            return (free(buffer), NULL);
-        if (ft_strchr(remainder, '\n'))
-            break ;
-        bytes_read = read(fd, buffer, BUFFER_SIZE);
-    }
-    free(buffer);
-    if (bytes_read < 0)
-        return (NULL);
-    return (remainder);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		buffer[bytes_read] = '\0';
+		if (remainder)
+			remainder = ft_strjoin(remainder, buffer);
+		else
+			remainder = ft_strdup(buffer);
+		if (!remainder)
+			return (free(buffer), NULL);
+		if (ft_strchr(remainder, '\n'))
+			break ;
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	}
+	free(buffer);
+	if (bytes_read < 0)
+		return (NULL);
+	return (remainder);
 }
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *remainder = NULL;
+	static char *remainder = NULL;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-
-    remainder = ft_read_and_join(fd, remainder);
-    if (!remainder || remainder[0] == '\0')
-    {
-        free(remainder);
-        remainder = NULL;
-        return (NULL);
-    }
-    return (extract_line(&remainder));
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	remainder = ft_read_and_join(fd, remainder);
+	if (!remainder || remainder[0] == '\0')
+	{
+		free(remainder);
+		remainder = NULL;
+		return (NULL);
+	}
+	return (extract_line(&remainder));
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,79 +1,96 @@
 #include "get_next_line.h"
 
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-    size_t  len = 0;
-    while (s[len])
-        len++;
-    return (len);
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
 
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-    char    *new_str;
-    size_t  i = 0, j = 0;
-    size_t  len1 = ft_strlen(s1);
-    size_t  len2 = ft_strlen(s2);
+	char	*new_str;
+	size_t		i;
+	size_t		j;
 
-    new_str = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (!new_str)
-    {
-        free(s1);
-        return (NULL);
-    }
-    while (i < len1)
-    {
-        new_str[i] = s1[i];
-        i++;
-    }
-    while (j < len2)
-    {
-        new_str[i + j] = s2[j];
-        j++;
-    }
-    new_str[i + j] = '\0';
-    free(s1);
-    return (new_str);
+	new_str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!new_str)
+	{
+		free(s1);
+		return (NULL);
+	}
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+    while (j < ft_strlen(s2))
+	{
+		new_str[ft_strlen(s1) + j] = s2[j];
+		j++;
+	}
+	new_str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (new_str);
 }
 
-char    *ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-    size_t  len = ft_strlen(s);
-    char    *dup = malloc(len + 1);
-    if (!dup)
-        return (NULL);
-    for (size_t i = 0; i <= len; i++)
-        dup[i] = s[i];
-    return (dup);
+	size_t		len;
+	size_t		i;
+	char	*dup;
+
+	len = ft_strlen(s);
+	dup = malloc(len + 1);
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i <= len)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	return (dup);
 }
 
-char    *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-    while (*s)
-    {
-        if (*s == (char)c)
-            return ((char *)s);
-        s++;
-    }
-    if (c == '\0')
-        return ((char *)s);
-    return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
 }
 
-char    *ft_substr(const char *s, size_t start, size_t len)
+char	*ft_substr(const char *s, size_t start, size_t len)
 {
-    size_t  sub_len = ft_strlen(s) - start;
-    char    *sub;
+	size_t		sub_len;
+	size_t		i;
+	char	*sub;
 
-    if (start >= ft_strlen(s))
-        return (ft_strdup(""));
-    if (sub_len > len)
-        sub_len = len;
-    sub = malloc(sub_len + 1);
-    if (!sub)
-        return (NULL);
-    for (size_t i = 0; i < sub_len; i++)
-        sub[i] = s[start + i];
-    sub[sub_len] = '\0';
-    return (sub);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	sub_len = ft_strlen(s) - start;
+	if (sub_len > len)
+		sub_len = len;
+	sub = malloc(sub_len + 1);
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < sub_len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[sub_len] = '\0';
+	return (sub);
 }
